f60ae91d3c99aafeacc58a8a2d2dca7a7be457f3
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2021-01-27 18:39:53 +0000

    ParkingLotsControllerTest, ParkingSpotsControllerTest and ReservationsControllerTest added to PrivateParkAPI

---
+++ PrivateParkAPI/testPrivateParkAPI/ReservationsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using PrivateParkAPI.Models;
+using testProject;
+using PrivateParkAPI.Controllers;
+
+namespace testPrivateParkAPI
+{
+    public class ReservationsControllerTest
+    {
+
+        [Fact]
+        public async Task GetAllReservationsAsync_ShouldReturnAllReservationsAsync()
+        {
+            // Arrange 
+            var dbName = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name;
+            //Arrange
+            var TestContext = TodoContextMocker.GetPrivateParkContext(dbName);
+            var theController = new ReservationsController(TestContext);
+            //Act
+            var result = await theController.GetReservations();
+
+            //Assert
+            var items = Assert.IsType<List<Reservation>>(result.Value);
+            Assert.Equal(8, items.Count);
+        }
+    }
+}
