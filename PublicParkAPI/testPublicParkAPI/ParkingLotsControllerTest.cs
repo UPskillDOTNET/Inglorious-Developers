3dc9700ee2874dcae95786828667c6c130fc555e
caiocreis <caiocruzeiror@upskill.pt>
2021-01-28 09:30:43 +0000

    New controllers added totestPublic

---
+++ PublicParkAPI/testPublicParkAPI/ParkingLotsControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using PublicParkAPI.Models;
+using testProject;
+using PublicParkAPI.Controllers;
+
+namespace testPublicParkAPI
+{
+    class ParkingLotsControllerTest
+    {
+    }
+}
---
+++ PublicParkAPI/testPublicParkAPI/ReservationsControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using PublicParkAPI.Models;
+using testProject;
+using PublicParkAPI.Controllers;
+namespace testPublicParkAPI
+{
+    class ReservationsControllerTest
+    {
+    }
+}
