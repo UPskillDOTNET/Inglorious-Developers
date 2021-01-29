ae3a668dc011b97b4c7b9b28d604ff6c27a86e8f
Vaazch <joaom.vodafone@upskill.pt>
2021-01-29 13:59:28 +0000

    authorization on public api correction

--- PublicParkAPI/PublicParkAPI/Controllers/ParkingLotsController.cs
+++ PublicParkAPI/PublicParkAPI/Controllers/ParkingLotsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +10,7 @@ using PublicParkAPI.Data;
 using PublicParkAPI.Models;
 
 namespace PublicParkAPI.Controllers
-{
+{   [Authorize]
     [Route("api/publicLots")]
     [ApiController]
     public class ParkingLotsController : ControllerBase
--- PublicParkAPI/PublicParkAPI/Controllers/ReservationsController.cs
+++ PublicParkAPI/PublicParkAPI/Controllers/ReservationsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using PublicParkAPI.Models;
 
 namespace PublicParkAPI.Controllers
 {
+    [Authorize]
     [Route("api/reservations")]
     [ApiController]
     public class ReservationsController : ControllerBase
