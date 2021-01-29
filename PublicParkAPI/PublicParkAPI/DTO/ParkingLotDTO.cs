04979fa565c05112db9424d4e3d990de9d14fedb
TiagoAzevedoUpSkill <tiagomina.azevedo@upskill.pt>
2021-01-29 14:55:42 +0000

    Testing Branch

---
+++ PublicParkAPI/PublicParkAPI/DTO/ParkingLotDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PublicParkAPI.DTO
+{
+    public class ParkingLotDTO
+    {
+
+        public int parkingLotID { get; set; }
+       
+        public string name { get; set; }
+        
+        public string municipality { get; set; }
+        
+        public string location { get; set; }
+ 
+        public int capacity { get; set; }
+
+        public DateTime openingTime { get; set; }
+
+        public DateTime closingTime { get; set; }
+    }
+}
