5237b0449ae00f67f344d42673bdf3d0cfb8ab98
Sergio Pinto <sergio.valente.pinto@upskill.pt>
2021-01-29 19:25:49 +0000

    DTOs created

---
+++ PrivateParkAPI/PrivateParkAPI/DTO/ParkingSpotDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PrivateParkAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateParkAPI.DTO {
+    public class ParkingSpotDTO {
+        public string parkingSpotID { get; set; }
+        public decimal priceHour { get; set; }
+        public bool isPrivate { get; set; }
+        public int ParkingLotID { get; set; }
+        public ParkingLot ParkingLot { get; set; }
+    }
+}
