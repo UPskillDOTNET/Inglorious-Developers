6b18f7e856bb71ddca76b30212a9fa1e0f96e302
Vaazch <joaom.vodafone@upskill.pt>
2021-02-02 10:16:05 +0000

    Validator Created for parkingLot parkingSpot Reservation

---
+++ PrivateParkAPI/PrivateParkAPI/Utils/ParkingLotValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using PrivateParkAPI.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivateParkAPI.Utils
+{
+    public class ParkingLotValidator: AbstractValidator<ParkingLotDTO>
+    {
+        public ParkingLotValidator()
+        {
+            RuleFor(ParkingLotDTO => ParkingLotDTO.name).MinimumLength(3).MinimumLength(24);
+            RuleFor(ParkingLotDTO => ParkingLotDTO.companyOwner).MinimumLength(3).MinimumLength(24);
+            RuleFor(ParkingLotDTO => ParkingLotDTO.location).MinimumLength(3);
+            RuleFor(ParkingLotDTO => ParkingLotDTO.capacity).NotEmpty();
+            RuleFor(ParkingLotDTO => ParkingLotDTO.openingTime).NotEmpty();
+            RuleFor(ParkingLotDTO => ParkingLotDTO.closingTime).NotEmpty();
+        }
+
+
+
+
+        // name 
+        // companyOwner 
+        // location 
+        // capacity
+        //openingTime 
+        //closingTime 
+    }
+}
---
+++ PrivateParkAPI/PrivateParkAPI/Utils/ParkingSpotValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using PrivateParkAPI.DTO;
+
+namespace PrivateParkAPI.Utils
+{
+    public class ParkingSpotValidator : AbstractValidator<ParkingSpotDTO>
+    {
+        public ParkingSpotValidator()
+        {
+            RuleFor(ParkingSpotDTO => ParkingSpotDTO.priceHour).GreaterThan(0).LessThan(100);
+            RuleFor(ParkingSpotDTO => ParkingSpotDTO.isPrivate).NotEmpty();
+        }
+
+    }
+    
+    
+}
