45f28acb56219d3a683a88f708618a540302f279
caiocreis <caiocruzeiror@upskill.pt>
2021-02-04 11:54:51 +0000

    Added folders and models

---
+++ CentralAPI/DTO/ParkingLotDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralAPI.DTO
+{
+    public class ParkingLotDTO
+    {
+        public int parkingLotID { get; set; }
+        public string name { get; set; }
+        public string companyOwner { get; set; }
+        public string location { get; set; }
+        public int capacity { get; set; }
+        public DateTime openingTime { get; set; }
+        public DateTime closingTime { get; set; }
+    }
+}
