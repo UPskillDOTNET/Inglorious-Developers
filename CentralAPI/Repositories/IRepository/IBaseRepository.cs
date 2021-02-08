ebfbdf2eee603d98f543a351cac4fe6046bda750
caiocreis <caiocruzeiror@upskill.pt>
2021-02-08 17:05:40 +0000

    Added the repository logic, and parking lot Repository

---
+++ CentralAPI/Repositories/IRepository/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralAPI.Repositories.IRepository
+{
+    public interface IBaseRepository<TEntity> where TEntity: class, new()
+    {
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> Find(string id);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(TEntity entity);
+    }
+}
---
+++ CentralAPI/Repositories/Repository/BaseRepository.cs
@@ -0,0 +1,101 @@
+﻿using CentralAPI.Data;
+using CentralAPI.Repositories.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralAPI.Repositories.Repository
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity: class, new()
+    {
+        protected readonly CentralParkContext CentralParkContext;
+        public BaseRepository(CentralParkContext centralContext)
+        {
+            CentralParkContext = centralContext;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            try
+            {
+                return CentralParkContext.Set<TEntity>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retieve entities: {ex.Message}");
+            }
+        }
+        public async Task<TEntity> Find(string id)
+        {
+            try
+            {
+                return await CentralParkContext.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retieve entities: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(AddAsync)} entity must not be null");
+            }
+
+            try
+            {
+                await CentralParkContext.AddAsync(entity);
+                await CentralParkContext.SaveChangesAsync();
+
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be saved: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(UpdateAsync)} entity must not be null");
+            }
+
+            try
+            {
+                CentralParkContext.Update(entity);
+                await CentralParkContext.SaveChangesAsync();
+
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be updated: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> DeleteAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(DeleteAsync)} entity must not be null");
+            }
+
+            try
+            {
+                CentralParkContext.Remove(entity);
+                await CentralParkContext.SaveChangesAsync();
+
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be updated: {ex.Message}");
+            }
+        }
+    }
+}
