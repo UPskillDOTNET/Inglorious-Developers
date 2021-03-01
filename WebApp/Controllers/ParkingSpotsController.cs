c0543cbb4f326dd76e8ef2be049cf83f42d76b6b
Sergio Pinto <sergio.valente.pinto@upskill.pt>
2021-03-01 18:49:35 +0000

    ParkingSpots Index reviewed

--- WebApp/Controllers/ParkingSpotsController.cs
+++ WebApp/Controllers/ParkingSpotsController.cs
@@ -10,15 +10,17 @@ namespace WebApp.Controllers
     public class ParkingSpotsController : Controller {
 
         private readonly IParkingSpotService _parkingSpotService;
+        private readonly IParkingLotService _parkingLotService;
 
-        public ParkingSpotsController(IParkingSpotService parkingSpotService) 
+        public ParkingSpotsController(IParkingSpotService parkingSpotService, IParkingLotService parkingLotService) 
         {
             _parkingSpotService = parkingSpotService;
+            _parkingLotService = parkingLotService;
         }
 
         public async Task<ActionResult<IEnumerable<ParkingSpotDTO>>> Index(int id) {
             try {
-                ViewData["parkingLotId"] = id; 
+                ViewBag.parkLotName = _parkingLotService.GetParkingLotById(id).Result.Value.name;
                 return View( _parkingSpotService.GetAllParkingSpots(id).Result.Value);
             } catch {
                 return NotFound();
