b4346dcac37dffa924b70cb055e2e72167098657
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2021-03-04 16:42:28 +0000

    Wallet Controller and Service created in WebApp

---
+++ WebApp/Services/Services/WalletService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DTO;
+using WebApp.Services.IServices;
+using WebApp.Services.Services.Utils;
+
+namespace WebApp.Services.Services
+{
+    public class WalletService
+    {
+    }
+}
