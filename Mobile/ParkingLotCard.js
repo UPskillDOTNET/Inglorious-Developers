135843f119e3e6c1665b119239d15c0f84501e56
TiagoAzevedoUpSkill <tiagomina.azevedo@upskill.pt>
2021-03-04 16:37:49 +0000

    Update to Modal

--- Mobile/ParkingLotCard.js
+++ Mobile/ParkingLotCard.js
@@ -126,16 +126,24 @@ const styles = StyleSheet.create({
     borderRadius: 7,
   },
   modalView: {
-    margin: 20,
     backgroundColor: "white",
     borderRadius: 20,
+    height: "40%",
     width: "100%",
-    height: "30%",
-    bottom: 0,
+    marginTop: "auto",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
     },
+  },
+  input: {
+    backgroundColor: "white",
+    borderRadius: 25,
+    padding: 8,
+    margin: 10,
+    width: 300,
+    textAlign: "center",
+    textTransform: "uppercase",
   },
 });
