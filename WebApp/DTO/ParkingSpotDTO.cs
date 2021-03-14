c0b3c3e290ca08c07b450af328f3bab35b09d252
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2021-03-14 20:27:58 +0000

    Spots Management Table Updated/Create New Button Added
    Changes to Display Names

--- WebApp/DTO/ParkingSpotDTO.cs
+++ WebApp/DTO/ParkingSpotDTO.cs
@@ -8,12 +8,17 @@ namespace WebApp.DTO
 {
     public class ParkingSpotDTO
     {
+        [Display(Name = "Designation")]
         public string parkingSpotID { get; set; }
         [DisplayFormat(DataFormatString = "{0:N}", ApplyFormatInEditMode = true)]
         [Range(0, 9999999999.999999)]
+        [Display(Name = "Price/Hour")]
         public decimal priceHour { get; set; }
+        [Display(Name = "Floor")]
         public int floor { get; set; }
+        [Display(Name ="Private")]
         public bool isPrivate { get; set; }
+        [Display(Name = "Covered")]
         public bool isCovered { get; set; }
         public int parkingLotID { get; set; }
     }
