36d37df1be6c2d63e5cb1ac4ffb4a13005db7609
Vaazch <joaom.vodafone@upskill.pt>
2021-03-14 01:58:29 +0000

    Updates

--- Mobile/Services/UserServices.js
+++ Mobile/Services/UserServices.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import qs from 'qs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API_CLIENT_ID = 'mobile.client';
 const API_CLIENT_SECRET = 'Secret';
 const API_BASE_URL = "https://localhost:5001";    
 
-async function  getAccessToken (credentials) {
+export default async function  getAccessToken (credentials) {
     const axiosConfig = {
         baseURL: API_BASE_URL,
         timeout: 30000,
@@ -26,10 +27,12 @@ async function  getAccessToken (credentials) {
     try {
         const result = await axios.post('/connect/token', qs.stringify(requestData), axiosConfig);  
         console.log("Token:",result.data);
-        console.log(result.data.access_token)
-        var userinfo = await getUserInfo(result.data.access_token);
-        console.log(userinfo);   
-        var reservations = getUserReservations(result.data.access_token, userinfo);
+        console.log(result.data.access_token);
+        var token = result.data.access_token;
+        await AsyncStorage.setItem("token", token);
+        var userinfo = await getUserInfo(token);
+        console.log(userinfo); 
+        var reservations = getUserReservations();
         console.log(reservations);
         return result.data;
         
@@ -37,18 +40,11 @@ async function  getAccessToken (credentials) {
         return err;
     }  
 
-}
-export default getAccessToken;
+};
 
 
-async function  getUserInfo(token){
 
-    const data ={
-        sub : String,
-        preferred_username : String,
-        name : String,
-        role : String
-    };
+ async function  getUserInfo(token){
 
     const axiosConfig = {
         baseURL: API_BASE_URL,
@@ -63,32 +59,32 @@ async function  getUserInfo(token){
         const result = await axios.get('/connect/userinfo', axiosConfig);  
         console.log(result.data);
         var userId = result.data.sub;
-        console.log(userId);        
+        await AsyncStorage.setItem("userID", userId);
+        console.log(userId);
         return userId;
-        
     } catch (err) {
         return err;
     }  
 
 }
 
-async function  getUserReservations(token, userinfo){
+ async function  getUserReservations(){
 
-    
+    var xeila1 = await AsyncStorage.getItem('accessToken');
     const axiosConfig = {
-        baseURL: "http://192.168.31.64:90",
+        baseURL: "http://192.168.1.3:90",
         timeout: 30000,
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token,
+            'Authorization': 'Bearer ' + xeila1,
         }
     };
 
-    var sub = userinfo;
-    console.log("sub",sub);
+    var xeila2 = await AsyncStorage.getItem('userID');
+    console.log("sub",xeila2);
     console.log("Config:", axiosConfig);
     try {
-        const result = await axios.get('/reservations/users/' + sub, axiosConfig);  
+        const result = await axios.get('/reservations/users/' + xeila2, axiosConfig);  
         console.log(result.data);
         var reservations = data;
         console.log(reservations);         
--- Mobile/screens/Tab1.js
+++ Mobile/screens/Tab1.js
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
 import { View, Text,Button ,StyleSheet } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
-import getAccessToken from "../Services/UserServices"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Tab1 = ({ navigation }) => {
   const { state } = useContext(AuthContext);
 
+ var xeila = AsyncStorage.getItem("userID");
+ console.log(xeila);
   return (
-    <View style={styles.master}>
+    <View style={styles.master} >
       <Text style={styles.header}>Tab1</Text>
-      <Text style={{ fontSize: 28 }}>Welcome, {state.username}</Text>
+      <Text style={{ fontSize: 28 }}>Welcome, {state.username} </Text>  
     </View>
   );
 };
